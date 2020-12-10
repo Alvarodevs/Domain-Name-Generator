5d8395020ff709260d87676b7c7ea09176b9000f
Alvarodevs <alvaro.develops@gmail.com>
2020-12-10 16:16:32 +0000

    Second commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var pronoun = ['the','our'];
-var adj = ['great', 'big' ];
-var noun = ['jogger','racoon'];
-var domain = ['.com', '.de'];
+var adj = ['great', 'big'];
+var noun = ['jogger','racoon', 'poderio', 'code'];
+var domain = ['.com', '.de', '.io'];
 
 function allDomains (pronoun, adj, noun, domain){
 
@@ -14,12 +14,24 @@ function allDomains (pronoun, adj, noun, domain){
             for (j = 0; j < noun.length; j++){
 
                 for (l = 0; l < domain.length; l++){
-                    result.push(pronoun[i] + adj[h] + noun[j] + domain[l]);}
+
+                    let newNoun = noun[j];
+
+                    let noDotDomain = domain[l].slice(1,domain[l].length);
+
+                    if (noun[j].endsWith(noDotDomain)) {
+
+                        //console.log("funciona",noun[j].replace(domain[l].slice(1,domain[l].length)," "))
+                        
+                        newNoun = noun[j].replace(noDotDomain,"");
+
+                        result.push(pronoun[i] + adj[h] + newNoun + domain[l])
+                    }
+                    else {result.push(pronoun[i] + adj[h] + noun[j] + domain[l]);}
+                }
             }
         }
     }
-    // return result.push(pronoun[i] + adj[h] + noun[j] + domain[l]);
     return result;
 }
 console.log(allDomains(pronoun, adj, noun, domain));
-
